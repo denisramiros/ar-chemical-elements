044c40d9b77852a5c8db02d5d47c0916f36de446
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2020-09-25 09:12:39 +0200

    Generate element nucleus

---
+++ Assets/Scripts/ChemicalElement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChemicalElement : MonoBehaviour
+{
+    [Header("Chemical element properties")]
+    public int number;
+    public float atomicMass;
+    public string electronConfiguration;
+
+    [Header("Particles prefabs")]
+    public Transform electronPrefab;
+    public Transform neutronPrefab;
+    public Transform protonPrefab;
+
+    void Start()
+    {
+        Generate();
+    }
+
+    void Generate()
+    {
+        int protons = number;
+        int electrons = number;
+        int neutrons = Mathf.RoundToInt(atomicMass) - protons;//N = Z - A
+        int nucleusRadius = 3;//TODO, should be lerped depending on particles in neutron
+
+        for (var i = 0; i < protons; i++)
+            Instantiate(protonPrefab, Random.insideUnitSphere * nucleusRadius, Quaternion.identity, this.transform);
+
+        for (var i = 0; i < neutrons; i++)
+            Instantiate(neutronPrefab, Random.insideUnitSphere * nucleusRadius, Quaternion.identity, this.transform);
+
+
+    }
+
+    void Update()
+    {
+
+    }
+}
