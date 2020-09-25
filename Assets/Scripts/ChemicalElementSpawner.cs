1e2473c41bd180840e5d633a02def0f33d397c33
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2020-09-25 11:55:30 +0200

    Add chemical element spawner

---
+++ Assets/Scripts/ChemicalElementSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChemicalElementSpawner : MonoBehaviour
+{
+    /// <summary>
+    /// Will spawn the chemical element
+    /// </summary>
+    /// <param name="number">"number" field in the JSON</param>
+    /// <param name="atomicMass">"atomic_mass" field in the json</param>
+    /// <param name="electronConfiguration">"electron_configuration" field in the JSON</param>
+    public void Spawn(int number, float atomicMass, string electronConfiguration)
+    {
+
+    }
+}
